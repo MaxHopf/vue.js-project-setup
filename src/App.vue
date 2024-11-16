c93dc031d818b9ef672b6c108d8ec768562ee2b0
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2024-11-16 23:32:40 +0200

    Add a vue component called 'Header.vue'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <h1>Hello from Vue.js Template</h1>
+  <Header />
 </template>
 
 <script>
+import Header from "./Header.vue";
 export default {
   name: "App",
+  components: {
+    Header,
+  },
 };
 console.log("Hello from vue.js console");
 </script>
---
+++ src/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <h2>Hello from a Vue Component</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
